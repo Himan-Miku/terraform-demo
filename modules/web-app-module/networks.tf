5bd4d9afff6876434028bdbc631dd36d80461c0f
Himan-Miku <himanshu28.mj@gmail.com>
2023-07-18 20:57:16 +0530

    refactor: names using variables

--- modules/web-app-module/networks.tf
+++ modules/web-app-module/networks.tf
@@ -7,7 +7,7 @@ data "aws_subnet_ids" "default_subnet" {
 }
 
 resource "aws_security_group" "instances" {
-  name = "instance-security-group"
+  name = "${var.app_name}-${var.environment_name}-instance-security-group"
 }
 
 resource "aws_security_group_rule" "allow_http_inbound" {
@@ -40,7 +40,7 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_target_group" "instances" {
-  name     = "example-target-group"
+  name     = "${var.app_name}-${var.environment_name}-tg"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default_vpc.id
@@ -86,7 +86,7 @@ resource "aws_lb_listener_rule" "instances" {
 
 
 resource "aws_security_group" "alb" {
-  name = "alb-security-group"
+  name = "${var.app_name}-${var.environment_name}-alb-security-group"
 }
 
 resource "aws_security_group_rule" "allow_alb_http_inbound" {
@@ -113,7 +113,7 @@ resource "aws_security_group_rule" "allow_alb_all_outbound" {
 
 
 resource "aws_lb" "load_balancer" {
-  name               = "web-app-lb"
+  name               = "${var.app_name}-${var.environment_name}-web-app-lb"
   load_balancer_type = "application"
   subnets            = data.aws_subnet_ids.default_subnet.ids
   security_groups    = [aws_security_group.alb.id]
